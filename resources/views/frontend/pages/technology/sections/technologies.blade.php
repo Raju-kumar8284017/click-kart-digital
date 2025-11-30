b2c5a82a0a58ea23e47c354dc85d013a6dc3f86b
Raju Kumar <kumarraj019876@gmail.com>
2025-12-01 02:44:12 +0530

    texc

---
+++ resources/views/frontend/pages/technology/sections/technologies.blade.php
@@ -0,0 +1,376 @@
+<div class="container py-5 tech-info-container">
+
+    <h2 class="text-center fw-bold mb-5 text-white">Technologies We Work With</h2>
+
+    <!-- 3 ROWS OF TECHNOLOGY ICONS -->
+    <div class="row g-4 justify-content-center">
+
+        <!-- Laravel -->
+        <div class="col-4 col-md-2 text-center">
+            <div class="tech-icon" data-bs-toggle="collapse" data-bs-target="#laravelInfo">
+                <img src="{{ asset('font_dist/img/icons/laravel.svg') }}" class="icon-img">
+                <p class="mt-2">Laravel</p>
+            </div>
+        </div>
+
+        <!-- React -->
+        <div class="col-4 col-md-2 text-center">
+            <div class="tech-icon" data-bs-toggle="collapse" data-bs-target="#reactInfo">
+                <img src="{{ asset('font_dist/img/icons/react.png') }}" class="icon-img">
+                <p class="mt-2">React</p>
+            </div>
+        </div>
+
+        <!-- WordPress -->
+        <div class="col-4 col-md-2 text-center">
+            <div class="tech-icon" data-bs-toggle="collapse" data-bs-target="#wpInfo">
+                <img src="{{ asset('font_dist/img/icons/wordpress.svg') }}" class="icon-img">
+                <p class="mt-2">WordPress</p>
+            </div>
+        </div>
+
+        <!-- Flutter -->
+        <div class="col-4 col-md-2 text-center">
+            <div class="tech-icon" data-bs-toggle="collapse" data-bs-target="#flutterInfo">
+                <img src="{{ asset('font_dist/img/icons/flutter.png') }}" class="icon-img">
+                <p class="mt-2">Flutter</p>
+            </div>
+        </div>
+
+        <!-- PHP -->
+        <div class="col-4 col-md-2 text-center">
+            <div class="tech-icon" data-bs-toggle="collapse" data-bs-target="#phpInfo">
+                <img src="{{ asset('font_dist/img/icons/php.svg') }}" class="icon-img">
+                <p class="mt-2">PHP</p>
+            </div>
+        </div>
+
+        <!-- Python -->
+        <div class="col-4 col-md-2 text-center">
+            <div class="tech-icon" data-bs-toggle="collapse" data-bs-target="#pythonInfo">
+                <img src="{{ asset('font_dist/img/icons/python.svg') }}" class="icon-img">
+                <p class="mt-2">Python</p>
+            </div>
+        </div>
+
+        <!-- Java -->
+        <div class="col-4 col-md-2 text-center">
+            <div class="tech-icon" data-bs-toggle="collapse" data-bs-target="#javaInfo">
+                <img src="{{ asset('font_dist/img/icons/java.svg') }}" class="icon-img">
+                <p class="mt-2">Java</p>
+            </div>
+        </div>
+
+        <!-- AWS -->
+        <div class="col-4 col-md-2 text-center">
+            <div class="tech-icon" data-bs-toggle="collapse" data-bs-target="#awsInfo">
+                <img src="{{ asset('font_dist/img/icons/aws.svg') }}" class="icon-img">
+                <p class="mt-2">AWS</p>
+            </div>
+        </div>
+
+        <!-- MySQL -->
+        <div class="col-4 col-md-2 text-center">
+            <div class="tech-icon" data-bs-toggle="collapse" data-bs-target="#mysqlInfo">
+                <img src="{{ asset('font_dist/img/icons/mysql.png') }}" class="icon-img">
+                <p class="mt-2">MySQL</p>
+            </div>
+        </div>
+
+        <!-- MongoDB -->
+        <div class="col-4 col-md-2 text-center">
+            <div class="tech-icon" data-bs-toggle="collapse" data-bs-target="#mongoInfo">
+                <img src="{{ asset('font_dist/img/icons/mongo.svg') }}" class="icon-img">
+                <p class="mt-2">MongoDB</p>
+            </div>
+        </div>
+
+        <!-- Figma -->
+        <div class="col-4 col-md-2 text-center">
+            <div class="tech-icon" data-bs-toggle="collapse" data-bs-target="#figmaInfo">
+                <img src="{{ asset('font_dist/img/icons/figma.svg') }}" class="icon-img">
+                <p class="mt-2">Figma</p>
+            </div>
+        </div>
+
+        <!-- Shopify -->
+        <div class="col-4 col-md-2 text-center">
+            <div class="tech-icon" data-bs-toggle="collapse" data-bs-target="#shopifyInfo">
+                <img src="{{ asset('font_dist/img/icons/shopify.svg') }}" class="icon-img">
+                <p class="mt-2">Shopify</p>
+            </div>
+        </div>
+
+        <!-- Kotlin -->
+        <div class="col-4 col-md-2 text-center">
+            <div class="tech-icon" data-bs-toggle="collapse" data-bs-target="#kotlinInfo">
+                <img src="{{ asset('font_dist/img/icons/kotlin.svg') }}" class="icon-img">
+                <p class="mt-2">Kotlin</p>
+            </div>
+        </div>
+
+        <!-- Swift -->
+        <div class="col-4 col-md-2 text-center">
+            <div class="tech-icon" data-bs-toggle="collapse" data-bs-target="#swiftInfo">
+                <img src="{{ asset('font_dist/img/icons/swift.svg') }}" class="icon-img">
+                <p class="mt-2">Swift</p>
+            </div>
+        </div>
+
+        <!-- Angular -->
+        <div class="col-4 col-md-2 text-center">
+            <div class="tech-icon" data-bs-toggle="collapse" data-bs-target="#angularInfo">
+                <img src="{{ asset('font_dist/img/icons/angular.svg') }}" class="icon-img">
+                <p class="mt-2">Angular</p>
+            </div>
+        </div>
+
+        <!-- Node.js -->
+        <div class="col-4 col-md-2 text-center">
+            <div class="tech-icon" data-bs-toggle="collapse" data-bs-target="#nodeInfo">
+                <img src="{{ asset('font_dist/img/icons/node.svg') }}" class="icon-img">
+                <p class="mt-2">Node.js</p>
+            </div>
+        </div>
+
+        <!-- Vue.js -->
+        <div class="col-4 col-md-2 text-center">
+            <div class="tech-icon" data-bs-toggle="collapse" data-bs-target="#vueInfo">
+                <img src="{{ asset('font_dist/img/icons/vue.png') }}" class="icon-img">
+                <p class="mt-2">Vue.js</p>
+            </div>
+        </div>
+
+        <!-- AI Tools -->
+        <div class="col-4 col-md-2 text-center">
+            <div class="tech-icon" data-bs-toggle="collapse" data-bs-target="#aiInfo">
+                <img src="{{ asset('font_dist/img/icons/ai.png') }}" class="icon-img">
+                <p class="mt-2">AI Tools</p>
+            </div>
+        </div>
+
+        <!-- DevOps -->
+        <div class="col-4 col-md-2 text-center">
+            <div class="tech-icon" data-bs-toggle="collapse" data-bs-target="#devopsInfo">
+                <img src="{{ asset('font_dist/img/icons/devops.svg') }}" class="icon-img">
+                <p class="mt-2">DevOps</p>
+            </div>
+        </div>
+
+
+    </div>
+
+    <!-- CONTENT SECTIONS – ICON LEFT / TEXT RIGHT -->
+
+    <!-- Laravel Info -->
+    <div class="collapse tech-info mt-4" id="laravelInfo" data-bs-parent=".tech-info-container">
+        <div class="info-box">
+            <img src="{{ asset('font_dist/img/icons/laravel.png') }}" class="info-icon">
+            <div>
+                <h4>Laravel Development</h4>
+                <p>We build secure, scalable, and high-performance applications using Laravel. From APIs to enterprise systems, Laravel helps us deliver clean, maintainable, and fast web solutions.</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- React Info -->
+    <div class="collapse tech-info mt-4" id="reactInfo" data-bs-parent=".tech-info-container">
+        <div class="info-box">
+            <img src="{{ asset('font_dist/img/icons/react.png') }}" class="info-icon">
+            <div>
+                <h4>React Development</h4>
+                <p>React enables the creation of fast, dynamic, and responsive user interfaces. We build reusable components and modern front-end applications with smooth performance.</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- WordPress Info -->
+    <div class="collapse tech-info mt-4" id="wpInfo" data-bs-parent=".tech-info-container">
+        <div class="info-box">
+            <img src="{{ asset('font_dist/img/icons/wordpress.png') }}" class="info-icon">
+            <div>
+                <h4>WordPress Development</h4>
+                <p>From custom themes to plugin development, WordPress helps us build powerful, SEO-friendly websites and eCommerce stores with full customization capability.</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- Flutter Info -->
+    <div class="collapse tech-info mt-4" id="flutterInfo" data-bs-parent=".tech-info-container">
+        <div class="info-box">
+            <img src="{{ asset('font_dist/img/icons/flutter.png') }}" class="info-icon">
+            <div>
+                <h4>Flutter App Development</h4>
+                <p>Flutter allows us to create beautiful and fast mobile apps for both Android and iOS using one codebase. Smooth animations and native performance guaranteed.</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- PHP Info -->
+    <div class="collapse tech-info mt-4" id="phpInfo" data-bs-parent=".tech-info-container">
+        <div class="info-box">
+            <img src="{{ asset('font_dist/img/icons/php.png') }}" class="info-icon">
+            <div>
+                <h4>PHP Development</h4>
+                <p>We build powerful backends, custom CMS systems, portals, and dynamic applications using PHP. It is secure, fast, and perfect for custom business solutions.</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- Python Info -->
+    <div class="collapse tech-info mt-4" id="pythonInfo" data-bs-parent=".tech-info-container">
+        <div class="info-box">
+            <img src="{{ asset('font_dist/img/icons/python.png') }}" class="info-icon">
+            <div>
+                <h4>Python Development</h4>
+                <p>Python helps us build AI tools, automation systems, APIs, data processing apps, and scalable backend services using frameworks like Django and Flask.</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- Java Info -->
+    <div class="collapse tech-info mt-4" id="javaInfo" data-bs-parent=".tech-info-container">
+        <div class="info-box">
+            <img src="{{ asset('font_dist/img/icons/java.png') }}" class="info-icon">
+            <div>
+                <h4>Java Development</h4>
+                <p>Java is ideal for enterprise-grade applications, banking systems, secure platforms, and high-performance backend services used by millions of users.</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- AWS Info -->
+    <div class="collapse tech-info mt-4" id="awsInfo" data-bs-parent=".tech-info-container">
+        <div class="info-box">
+            <img src="{{ asset('font_dist/img/icons/aws.png') }}" class="info-icon">
+            <div>
+                <h4>AWS Cloud Services</h4>
+                <p>We deploy, scale, and manage cloud infrastructure using Amazon Web Services, ensuring high availability, security, and global performance for applications.</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- MySQL Info -->
+    <div class="collapse tech-info mt-4" id="mysqlInfo" data-bs-parent=".tech-info-container">
+        <div class="info-box">
+            <img src="{{ asset('font_dist/img/icons/mysql.png') }}" class="info-icon">
+            <div>
+                <h4>MySQL Database</h4>
+                <p>MySQL is a powerful relational database used for fast, secure, and structured data storage. We design scalable and optimized databases for all applications.</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- MongoDB Info -->
+    <div class="collapse tech-info mt-4" id="mongoInfo" data-bs-parent=".tech-info-container">
+        <div class="info-box">
+            <img src="{{ asset('font_dist/img/icons/mongo.png') }}" class="info-icon">
+            <div>
+                <h4>MongoDB NoSQL Database</h4>
+                <p>We use MongoDB for modern, scalable NoSQL applications that require high-speed data handling, flexible schema design, and real-time processing.</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- Figma Info -->
+    <div class="collapse tech-info mt-4" id="figmaInfo" data-bs-parent=".tech-info-container">
+        <div class="info-box">
+            <img src="{{ asset('font_dist/img/icons/figma.png') }}" class="info-icon">
+            <div>
+                <h4>Figma UI/UX Design</h4>
+                <p>We design responsive, modern, and user-friendly interfaces using Figma. Perfect for prototypes, UI kits, product design, and brand identity systems.</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- Shopify Info -->
+    <div class="collapse tech-info mt-4" id="shopifyInfo" data-bs-parent=".tech-info-container">
+        <div class="info-box">
+            <img src="{{ asset('font_dist/img/icons/shopify.png') }}" class="info-icon">
+            <div>
+                <h4>Shopify Development</h4>
+                <p>We build high-converting eCommerce stores using Shopify, with custom themes, checkout optimization, product setup, and advanced automation.</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- Kotlin Info -->
+    <div class="collapse tech-info mt-4" id="kotlinInfo" data-bs-parent=".tech-info-container">
+        <div class="info-box">
+            <img src="{{ asset('font_dist/img/icons/kotlin.png') }}" class="info-icon">
+            <div>
+                <h4>Kotlin App Development</h4>
+                <p>Kotlin is our preferred language for modern and secure Android applications. We build high-performance, user-friendly mobile apps with seamless UI and scalability.</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- Swift Info -->
+    <div class="collapse tech-info mt-4" id="swiftInfo" data-bs-parent=".tech-info-container">
+        <div class="info-box">
+            <img src="{{ asset('font_dist/img/icons/swift.png') }}" class="info-icon">
+            <div>
+                <h4>Swift iOS Development</h4>
+                <p>We develop premium iOS apps with Swift—fast performance, clean UI, robust architecture, and perfect compatibility with the entire Apple ecosystem.</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- Angular Info -->
+    <div class="collapse tech-info mt-4" id="angularInfo" data-bs-parent=".tech-info-container">
+        <div class="info-box">
+            <img src="{{ asset('font_dist/img/icons/angular.png') }}" class="info-icon">
+            <div>
+                <h4>Angular Development</h4>
+                <p>We create enterprise-grade web applications with Angular—featuring strong architecture, modular components, and lightning-fast performance.</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- Node.js Info -->
+    <div class="collapse tech-info mt-4" id="nodeInfo" data-bs-parent=".tech-info-container">
+        <div class="info-box">
+            <img src="{{ asset('font_dist/img/icons/node.png') }}" class="info-icon">
+            <div>
+                <h4>Node.js Backend Development</h4>
+                <p>Node.js helps us build real-time, scalable, and high-speed backend applications, APIs, dashboards, automation tools, and enterprise platforms.</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- Vue.js Info -->
+    <div class="collapse tech-info mt-4" id="vueInfo" data-bs-parent=".tech-info-container">
+        <div class="info-box">
+            <img src="{{ asset('font_dist/img/icons/vue.png') }}" class="info-icon">
+            <div>
+                <h4>Vue.js Development</h4>
+                <p>Vue.js provides a lightweight, flexible, and reactive environment for creating modern front-end applications with smooth UI and superb user experience.</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- AI Tools Info -->
+    <div class="collapse tech-info mt-4" id="aiInfo" data-bs-parent=".tech-info-container">
+        <div class="info-box">
+            <img src="{{ asset('font_dist/img/icons/ai.png') }}" class="info-icon">
+            <div>
+                <h4>AI & Automation Tools</h4>
+                <p>We develop AI solutions including chatbots, automation systems, machine learning models, OCR tools, predictive analytics, and AI-driven business intelligence.</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- DevOps Info -->
+    <div class="collapse tech-info mt-4" id="devopsInfo" data-bs-parent=".tech-info-container">
+        <div class="info-box">
+            <img src="{{ asset('font_dist/img/icons/devops.png') }}" class="info-icon">
+            <div>
+                <h4>DevOps & CI/CD</h4>
+                <p>We use Docker, Kubernetes, Jenkins, GitHub Actions, CI/CD pipelines, and monitoring tools to automate deployments, scaling, and cloud infrastructure.</p>
+            </div>
+        </div>
+    </div>
+
+
+
+</div>
