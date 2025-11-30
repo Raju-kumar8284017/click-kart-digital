e0c34ccb344e9695dedecaaef55cf4cc2d4ade14
Raju Kumar <kumarraj019876@gmail.com>
2025-12-01 03:08:30 +0530

    clr

--- resources/views/frontend/pages/technology/sections/technologies.blade.php
+++ resources/views/frontend/pages/technology/sections/technologies.blade.php
@@ -1,6 +1,6 @@
 <div class="container py-5">
 
-    <h2 class="text-center fw-bold mb-5">Technologies We Work With</h2>
+    <h2 class="text-center fw-bold mb-5 wave-text">Technologies We Work With</h2>
 
     <!-- FRONTEND TECH -->
     <h3 class="mb-3">Frontend Technologies</h3>
