0f7e4d9e3613ade544571aebd513ab066b743bfd
Raju Kumar <kumarraj019876@gmail.com>
2025-12-02 19:40:58 +0100

    why

--- resources/views/frontend/pages/home/section/brand.blade.php
+++ resources/views/frontend/pages/home/section/brand.blade.php
@@ -12,11 +12,11 @@
                         <div class="text-center mx-auto pb-1 wow fadeInUp" data-wow-delay="0.2s">
                             <h4 class="text-uppercase text-head head-sm pt-5">Elevate Your Brand With ClickKart</h4>
                             <p class="sub-head sub-head-sm mb-3">
-                                Recognised As The Leading Digital Marketing Company In Patna,
-                                We Have The Expertise To Drive Digital Growth With Transformation
-                                Strategies Tailored To Your Goals. Whether It Is A Design That Is
-                                Easy To Use, An Intuitive User Experience, Or Data-Driven
-                                Campaigns, We Focus On Delivering Results That Matter.
+                               Recognised as the leading digital marketing company in Patna, we have the expertise 
+                               to drive digital growth with transformation strategies tailored to your goals. Our 
+                               comprehensive digital marketing services ensure that whether it is a design that is 
+                               easy to use, an intuitive user experience, or data-driven campaigns, we focus on 
+                               delivering results that matter.
                             </p>
                         </div>
                          <div class="row my-2">
@@ -29,7 +29,7 @@
                                                 <b class="mx-2">Professional Support:</b>
                                             </div>
                                             <div class="col-md-8">
-                                                <span>Motta Offers Dedicated Support From Our Experienced Experts</span>
+                                                <span>Motta offers dedicated support from our experienced experts.</span>
                                             </div>
                                         </div>
                                     </div>
@@ -46,7 +46,7 @@
                                                 <b class="mx-2">Quality Solutions:</b>
                                             </div>
                                             <div class="col-md-8">
-                                                <span>We Are Committed To Providing First-Class Solutions Tailored To Your Needs.</span>
+                                                <span>W We are committed to providing first-class solutions tailored to your needs.</span>
                                             </div>
                                         </div>
                                     </div>
@@ -72,6 +72,80 @@
                          </div>
                     </div>
                 </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="col">
+                        <div class="text-center mx-auto pb-1 wow fadeInUp" data-wow-delay="0.2s">
+                            <h4 class="text-uppercase text-head head-sm pt-5">Unlock Your Business Potential with ClickKart</h4>
+                            <p class="sub-head sub-head-sm mb-3">
+                              Unlock the potential of your business with ClickKart, a compelling digital marketing 
+                              company in Patna that specializes in SEO services, pay-per-click services, and IT 
+                              solutions. Counted among the reliable digital marketing firms, our data-driven strategy 
+                              helps operators grow, improve online visibility, and achieve important achievements. 
+                              Our approach is simple: We strive to create transformative experiences that leave a 
+                              lasting impression.
+                            </p>
+                        </div>
+                         <div class="row my-2">
+                            <div class="col">
+                                <div class="card wow fadeInUp"  data-wow-delay="0.4s">
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="col-md-4 d-inline-flex">
+                                                <i class="fa fa-handshake fa-2x brand-elevate-icon"></i>
+                                                <b class="mx-2">Captivating campaigns:</b>
+                                            </div>
+                                            <div class="col-md-8">
+                                                <span>Engage your audience with attention-grabbing design and messaging.</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                         </div>
+                         <div class="row my-2">
+                            <div class="col">
+                                <div class="card wow fadeInUp"  data-wow-delay="0.6s">
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="col-md-4 d-inline-flex">
+                                                <i class="fa fa-star fa-2x brand-elevate-icon"></i>
+                                                <b class="mx-2">Simplifying the User Experience:</b>
+                                            </div>
+                                            <div class="col-md-8">
+                                                <span>From Strategy to Execution We value user friendliness and results.</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                         </div>
+                         <div class="row my-2">
+                            <div class="col">
+                                <div class="card wow fadeInUp"  data-wow-delay="0.8s">
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="col-md-4 d-inline-flex">
+                                                <i class="fa fa-thumbs-up fa-2x brand-elevate-icon"></i>
+                                                <b class="mx-2">Commitment to Safety:</b>
+                                            </div>
+                                            <div class="col-md-8">
+                                                <span> We are committed to quality in every step of the process.</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                         </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 text-center d-flex justify-content-center align-items-center">
+                <img src="{{ asset('font_dist/img/p.png') }}" class="img-fluid" width="100%" height="100%" alt="">
             </div>
         </div>
     </div>
