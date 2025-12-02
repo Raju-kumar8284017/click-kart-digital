8094a1073b52976bef230d219a65b9d05ff8b9ff
Raju Kumar <kumarraj019876@gmail.com>
2025-12-02 20:11:56 +0100

    choogen by

--- resources/views/frontend/common/choosen.blade.php
+++ resources/views/frontend/common/choosen.blade.php
@@ -11,11 +11,15 @@
         <div class="row">
             <div class="col my-2">
                 <div class="owl-carousel owl-theme">
-                    <div class="item"><img src="{{ asset('font_dist/img/choosen/service-1.jpg') }}" alt="Slide 1"></div>
-                    <div class="item"><img src="{{ asset('font_dist/img/choosen/service-2.jpg') }}" alt="Slide 2"></div>
-                    <div class="item"><img src="{{ asset('font_dist/img/choosen/service-3.jpg') }}" alt="Slide 3"></div>
-                    <div class="item"><img src="{{ asset('font_dist/img/choosen/service-4.jpg') }}" alt="Slide 4"></div>
-                    <div class="item"><img src="{{ asset('font_dist/img/choosen/service-3.jpg') }}" alt="Slide 5"></div>
+                    <div class="item"><img src="{{ asset('font_dist/img/choosen/1st.jpg') }}" alt="Slide 1"></div>
+                    <div class="item"><img src="{{ asset('font_dist/img/choosen/2nd.jpg') }}" alt="Slide 2"></div>
+                    <div class="item"><img src="{{ asset('font_dist/img/choosen/3rd.jpg') }}" alt="Slide 3"></div>
+                    <div class="item"><img src="{{ asset('font_dist/img/choosen/4th.jpg') }}" alt="Slide 4"></div>
+                    <div class="item"><img src="{{ asset('font_dist/img/choosen/5th.jpg') }}" alt="Slide 5"></div>
+                    <div class="item"><img src="{{ asset('font_dist/img/choosen/6th.jpg') }}" alt="Slide 5"></div>
+                    <div class="item"><img src="{{ asset('font_dist/img/choosen/7th.jpg') }}" alt="Slide 5"></div>
+                    <div class="item"><img src="{{ asset('font_dist/img/choosen/8th.jpg') }}" alt="Slide 5"></div>
+                    <div class="item"><img src="{{ asset('font_dist/img/choosen/9th.jpg') }}" alt="Slide 5"></div>
                 </div>
             </div>
         </div>
