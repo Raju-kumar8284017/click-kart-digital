e38748bd8c371cda14eaf37d43bbf8e8317f2397
Raju Kumar <kumarraj019876@gmail.com>
2025-12-04 19:13:22 +0530

    seo

--- resources/views/frontend/pages/services/email-campaign/index.blade.php
+++ resources/views/frontend/pages/services/email-campaign/index.blade.php
@@ -1,5 +1,11 @@
 @extends('frontend.layouts.master')
 
+@section('meta_title', 'Best digital marketing & Website Designing Company in Patna | Click Kart Digital')
+
+@section('meta_description', 'Click Kart Digital provides professional website designing, web development, and digital marketing services in Patna.')
+
+@section('meta_keywords', 'digital marketing in Patna, website development, website design, click kart digital, IT company')
+
 @section('content')
     
 <!--email campaign banner-->
