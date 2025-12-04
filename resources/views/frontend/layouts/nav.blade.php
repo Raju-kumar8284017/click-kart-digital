202e52806c0752b3ac226ade3c68c2166eb3e1ee
Raju Kumar <kumarraj019876@gmail.com>
2025-12-04 18:16:25 +0100

    g

--- resources/views/frontend/layouts/nav.blade.php
+++ resources/views/frontend/layouts/nav.blade.php
@@ -22,7 +22,7 @@
                                             <h6 class="text-uppercase mb-3">Marketing</h6>
                                             <a href="{{ route('services.seo.index') }}" class="dropdown-item">SEO</a>
                                             <a href="{{ route('services.social-media.index') }}" class="dropdown-item">Social Media</a>
-                                            <a href="{{ route('services.email-campaign.index') }}" class="dropdown-item">Email Campaigns</a>
+                                            <a href="{{ route('services.email-campaign.index') }}" class="dropdown-item">Email Marketing</a>
                                         </div>
                                         <div class="col-md-4">
                                             <h6 class="text-uppercase mb-3">Design Services</h6>
--- resources/views/frontend/pages/services/email-marketing/index.blade.php
+++ resources/views/frontend/pages/services/email-marketing/index.blade.php
@@ -9,7 +9,7 @@
 @section('content')
     
 <!--email campaign banner-->
-@include('frontend.pages.services.email-campaign.sections.banner')
+@include('frontend.pages.services.email-marketing.sections.banner')
 <!--email campaign banner end-->
 
 <!--choosen by-->
@@ -17,11 +17,11 @@
 <!--choosen by End-->
 
 <!-- what we do -->
-@include('frontend.pages.services.email-campaign.sections.what_we_do')
+@include('frontend.pages.services.email-marketing.sections.what_we_do')
 <!-- what we do End -->
 
 <!--Inculcate Social Media Into Marketing-->
-@include('frontend.pages.services.email-campaign.sections.include_social_media')
+@include('frontend.pages.services.email-marketing.sections.include_social_media')
 <!--Inculcate Social Media Into Marketing End-->
 
 @include('frontend.common.future')
